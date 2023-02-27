62c2458eaefba6097d879fcc1b8d49279801d600
msa <serg.ks@gmail.com>
2023-02-27 18:30:10 +0400

    fix: keys

--- src/config/consts.js
+++ src/config/consts.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 console.log('The test value is ' + process.env.ASSETS_URI);
 console.log('The test value is ' + process.env.JWT);
 
-let environment = process.env.NODE_ENV;
-
-if (environment === 'production') {
-  module.exports = require('./keys.prod');
-} else {
-  module.exports = require('./keys.dev');
-}
+// let environment = process.env.NODE_ENV;
+//
+// if (environment === 'production') {
+//   module.exports = require('./keys.prod');
+// } else {
+module.exports = require('./keys.dev');
+// }
--- src/config/keys.dev.js
+++ src/config/keys.dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongodbURL: 'mongodb://127.0.0.1:27017',
   jwt: 'dev-jwt',
-  assetsURI: 'http://127.0.0.1:3010'
+  assetsURI: 'https://vds.msaserj.ru'
 };
