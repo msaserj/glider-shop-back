11ead068ac3b50e07a15d2e337ff705769302cb9
msa <serg.ks@gmail.com>
2023-03-03 23:54:04 +0400

    test commit

--- src/config/consts.js
+++ src/config/consts.js
@@ -3,7 +3,6 @@ console.log('The test value is ' + process.env.ASSETS_URI);
 console.log('The test value is ' + process.env.JWT);
 
 // let environment = process.env.NODE_ENV;
-//
 // if (environment === 'production') {
 //   module.exports = require('./keys.prod');
 // } else {
